3d681ea683a2581600f52c5b3d3d564d858c3eb3
fady73 <fadykhayrat@gmail.com>
2020-08-20 04:27:23 +0200

    Updates

---
+++ precache-manifest.10f64c0e5df34693285e60134401df35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b3ad032dae74079438ce",
+    "url": "/myBlog-/static/css/main.f23c3073.chunk.css"
+  },
+  {
+    "revision": "b3ad032dae74079438ce",
+    "url": "/myBlog-/static/js/main.b3ad032d.chunk.js"
+  },
+  {
+    "revision": "c2085778500529834858",
+    "url": "/myBlog-/static/css/1.079802a2.chunk.css"
+  },
+  {
+    "revision": "c2085778500529834858",
+    "url": "/myBlog-/static/js/1.c2085778.chunk.js"
+  },
+  {
+    "revision": "e2d6d67a9e7eb2077abe",
+    "url": "/myBlog-/static/js/runtime~main.e2d6d67a.js"
+  },
+  {
+    "revision": "cdde484fd8483185defa4597b8ae3d86",
+    "url": "/myBlog-/index.html"
+  }
+];
