6a78f1cb86bf979376599a2eee806fdd9ae42962
fady <fadykhayrat@gmail.com>
2020-08-20 06:05:18 +0200

    feat

---
+++ build/precache-manifest.625c1326d7cafc1d327c0ecb691f4aa1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "06f96f1033b7ae77263e",
+    "url": "/static/css/main.f23c3073.chunk.css"
+  },
+  {
+    "revision": "06f96f1033b7ae77263e",
+    "url": "/static/js/main.06f96f10.chunk.js"
+  },
+  {
+    "revision": "198a1ddef21828d5d487",
+    "url": "/static/css/1.079802a2.chunk.css"
+  },
+  {
+    "revision": "198a1ddef21828d5d487",
+    "url": "/static/js/1.198a1dde.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f83868773c18db2684f033ef66270a20",
+    "url": "/index.html"
+  }
+];
