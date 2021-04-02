66d4bade94f7315494730adae5dcd067ebe4790d
fady <fadykhayrat@gmail.com>
2021-04-02 04:12:14 +0200

    feat(pagination) add pages

--- src/containers/posts/postIndex.js
+++ src/containers/posts/postIndex.js
@@ -1,118 +1,107 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
-import _ from 'lodash';
-import queryString from 'query-string';
-import {fetchPosts, deletePost} from '../../actions/postAction';
-import Post from '../../components/posts/post';
-import Loader from '../../components/loader';
-import SearchBar from '../../components/searchBar';
-import Pagination from '../../components/pagination';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import _ from "lodash";
+import queryString from "query-string";
+import { fetchPosts, deletePost } from "../../actions/postAction";
+import Post from "../../components/posts/post";
+import Loader from "../../components/loader";
+import SearchBar from "../../components/searchBar";
+import Pagination from "../../components/pagination";
 
-import ReactGA from 'react-ga';
+import ReactGA from "react-ga";
 
 class PostIndex extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentPage: 1,
+      postsPerPage: 10,
+    };
+  }
 
-	constructor(props) {
-		super(props);
-		this.state = {
-			currentPage: 1,
-			postsPerPage: 2
-		}
-	}
+  componentWillReceiveProps(newProps) {
+    // parse query string and set currenPage state accordingly
+    const parsed = queryString.parse(newProps.location.search).page;
+    if (parsed !== undefined) this.setState({ currentPage: parsed });
+    else {
+      this.setState({ currentPage: 1 });
+    }
+    ReactGA.pageview(this.props.location.pathname);
+  }
 
-	componentWillReceiveProps(newProps) {
-		// parse query string and set currenPage state accordingly
-		const parsed = queryString.parse(newProps.location.search).page;
-		if (parsed !== undefined)
-			this.setState({currentPage: parsed});
-		else {
-			this.setState({currentPage: 1});
-		}
-		ReactGA.pageview(this.props.location.pathname);  
+  componentDidMount() {
+    this.props.fetchPosts();
+    ReactGA.pageview(this.props.location.pathname);
+  }
 
-	}
-	
-	componentDidMount() {
-		this.props.fetchPosts();
-		ReactGA.pageview(this.props.location.pathname);  
+  handlePaginationClick(e) {
+    this.setState({ currentPage: e.target.id });
+    this.props.history.push(`/questions?page=${e.target.id}`);
+  }
 
-	}
+  handlePrevNextClick(e, pageNumbers, currentPage) {
+    const lastPage = _.last(pageNumbers);
 
+    if (currentPage > 1 && e.target.id === "Prev") {
+      let page = currentPage - 1;
+      this.setState({ currentPage: page });
+      this.props.history.push(`/questions?page=${page}`);
+    } else if (currentPage < lastPage && e.target.id === "Next") {
+      let page = Number(currentPage) + 1;
+      this.setState({ currentPage: page });
+      this.props.history.push(`/questions?page=${page}`);
+    }
+  }
 
-	handlePaginationClick(e) {
-		this.setState({currentPage: e.target.id});	
-		this.props.history.push(`/questions?page=${e.target.id}`);
-	}
+  renderPosts() {
+    const { currentPage, postsPerPage } = this.state;
+    const endIndex = this.state.currentPage * postsPerPage;
+    const startIndex = endIndex - postsPerPage;
+    const postsToShow = _.slice(this.props.posts, startIndex, endIndex);
 
-	handlePrevNextClick(e, pageNumbers, currentPage) {
-		const lastPage = _.last(pageNumbers);
+    return _.map(postsToShow, (post, key) => {
+      return <Post key={key} post={post} />;
+    });
+  }
 
-		if (currentPage > 1 && e.target.id === 'Prev') {
-			let page = currentPage - 1;
-			this.setState({currentPage: page});
-			this.props.history.push(`/questions?page=${page}`);
-		}
-		else if (currentPage < lastPage && e.target.id === 'Next') {
-			let page = Number(currentPage) + 1;
-			this.setState({currentPage: page});
-			this.props.history.push(`/questions?page=${page}`);
-		}
-	}
+  render() {
+    const { posts, isFetched } = this.props;
 
+    if (!isFetched) return <Loader />;
 
-	renderPosts() {
-		const {currentPage, postsPerPage} = this.state;
-		const endIndex = this.state.currentPage * postsPerPage;
-		const startIndex = endIndex - postsPerPage;
-		const postsToShow = _.slice(this.props.posts, startIndex, endIndex);
-		
-		return _.map(postsToShow, (post, key) => {
-			return <Post 
-						key={key}
-						post={post}
-					/>
-			});
-	}
+    if (!posts.length && isFetched)
+      return (
+        <div className="post-index d-flex justify-content-center">
+          There are no questions yet.
+        </div>
+      );
 
-	render() {
-		const {posts, isFetched} = this.props;		
+    const pageNumbers = [];
+    for (let i = 1; i <= Math.ceil(posts.length / this.state.postsPerPage); i++)
+      pageNumbers.push(i);
 
-		if (!isFetched)
-			return <Loader />
-		
-		if (!posts.length && isFetched)
-			return <div className="post-index d-flex justify-content-center">There are no questions yet.</div>
-		
-	    const pageNumbers = [];
-		for (let i = 1; i <= Math.ceil(posts.length / this.state.postsPerPage) ; i++)
-			pageNumbers.push(i);
+    return (
+      <div className="post-index">
+        <div className="posts">{this.renderPosts()}</div>
 
-		return (
-
-				<div className="post-index">
-							
-					<div className="posts">
-						{this.renderPosts()}
-					</div>
-
-					<div className="pageNumbers text-center">
-						<Pagination 
-							currentPage={this.state.currentPage} 
-							pageNumbers={pageNumbers} 
-							handlePrevNextClick={this.handlePrevNextClick.bind(this)}
-							handlePaginationClick={this.handlePaginationClick.bind(this)}
-						/>
-					</div>
-				</div>
-		);
-	}
+        <div className="pageNumbers text-center">
+          <Pagination
+            currentPage={this.state.currentPage}
+            pageNumbers={pageNumbers}
+            handlePrevNextClick={this.handlePrevNextClick.bind(this)}
+            handlePaginationClick={this.handlePaginationClick.bind(this)}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 function mapStateToProps(state) {
-	return {
-		posts: state.posts.list,
-		isFetched: state.posts.isFetched
-	};
+  return {
+    posts: state.posts.list,
+    isFetched: state.posts.isFetched,
+  };
 }
 
-export default connect(mapStateToProps, {fetchPosts, deletePost})(PostIndex);
+export default connect(mapStateToProps, { fetchPosts, deletePost })(PostIndex);
