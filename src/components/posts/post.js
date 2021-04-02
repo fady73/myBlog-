b84508bc10959a876a65d65b6a6ed03cedf0724e
fady <fadykhayrat@gmail.com>
2021-04-02 04:22:09 +0200

    add react hamlet

--- src/components/posts/post.js
+++ src/components/posts/post.js
@@ -1,32 +1,48 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
-import LinesEllipsis from 'react-lines-ellipsis'
-import PropTypes from 'prop-types';
+import React from "react";
+import { Link } from "react-router-dom";
+import LinesEllipsis from "react-lines-ellipsis";
+import PropTypes from "prop-types";
+import { Helmet } from "react-helmet";
 
 const Post = (props) => {
-	const {post} = props;
-	const date = new Date(post.createdAt);
-	const formatedDate = date.toJSON();
+  const { post } = props;
+  const date = new Date(post.createdAt);
+  const formatedDate = date.toJSON();
 
-	return(
-		<div className="row">
-			<div className="post col-sm-10 col-sm-offset-1 animated fadeIn">
-				<div className="d-flex w-100 justify-content-between">
-					<Link className="post-title" to={`/question/${post.id}`}>{post.title}</Link>
-				</div>
-				<div className="post-content">
-					<LinesEllipsis text={post.content} maxLine='2' ellipsis='...' trimRight />
-				</div> 
-				<div className="post-info">
-					<i className="fa fa-calendar"></i>{formatedDate.slice(0, 10)}
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="row">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>thinkandask</title>
+        <link rel="canonical" href="https://thinkandask.herokuapp.com/" />
+        <meta name="description" content="Tutorial for React Helmet fady" />
+        <meta name="theme-color" content="#E6E6FA" />
+      </Helmet>
+      <div className="post col-sm-10 col-sm-offset-1 animated fadeIn">
+        <div className="d-flex w-100 justify-content-between">
+          <Link className="post-title" to={`/question/${post.id}`}>
+            {post.title}
+          </Link>
+        </div>
+        <div className="post-content">
+          <LinesEllipsis
+            text={post.content}
+            maxLine="2"
+            ellipsis="..."
+            trimRight
+          />
+        </div>
+        <div className="post-info">
+          <i className="fa fa-calendar"></i>
+          {formatedDate.slice(0, 10)}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Post;
 
 Post.propTypes = {
-	post: PropTypes.object.isRequired
+  post: PropTypes.object.isRequired,
 };
