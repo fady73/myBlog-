db220d5812df00bb90d40fecd52a32be8c0feda0
fady khayrat <fadykhayrat@gmail.com>
2021-07-13 02:07:20 +0200

    Update my.js

--- src/my.js
+++ src/my.js
@@ -1,10 +1,1 @@
-window.fcSettings = {
-    token: "470644dd-35ce-4c73-90b8-1fd345569389",
-    host: "https://wchat.freshchat.com",
-    onInit: function() {
-    	window.fcWidget.on("widget:closed", function() {
-        console.log('widget was closed');
-        document.getElementById('fc_frame').style.display = 'none';
-      });
-    }
-  };
+console.log("hello")
